3d32bc77e0ccc3b2736519f10084b9a10f77ce62
Djordjije <milovanovicdjordjije@gmail.com>
2025-10-16 10:42:10 +0200

    updated edge cases

--- emailFinder.py
+++ emailFinder.py
@@ -1,14 +1,11 @@
 #! /usr/bin/python3
 import re, pyperclip
 
-emailRegex = re.compile(r'\w+@\w+\.[a-zA-Z]{2,4}')
-
-#napisi i za fon i dodaj u clipboard
-phoneRegex = re.compile(r''' ''')
+emailRegex = re.compile(r"[\w+.'\"-]+@\w+\.[a-zA-Z]{2,4}")
 
 text = pyperclip.paste()
 
 allMail = emailRegex.findall(text)
 mailText  = '\n'.join(allMail)
 pyperclip.copy(mailText)
-print(mailText)
+print("Successfully extracted email addresses to clipboard")
